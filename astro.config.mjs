001e8635a94678571fc1a371c5173ea2d188e38a
devSaifur <saifur2akash@gmail.com>
2025-06-20 18:17:18 +0600

    50%

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://agency-template.vbartalis.dev",
-  base: "",
+  site: 'https://agency-template.vbartalis.dev',
+
+  base: '',
+
+  output: 'server',
+
   vite: {
-    plugins: [tailwindcss()],
-  },
-});
+    plugins: [tailwindcss()]
+  }
+})
